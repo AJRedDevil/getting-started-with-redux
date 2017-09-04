3ae8b8c82d3cefc4279d0239ea434cea661b6b67
AJRedDevil <ajan.shresh@gmail.com>
2017-09-04 15:47:50 +0545

    Test for todo list TOGGLE_TODO action

--- playground/example-todolist.js
+++ playground/example-todolist.js
@@ -12,6 +12,16 @@ const todos = (state = [], action) => {
                     completed: false
                 }
             ];
+        case 'TOGGLE_TODO':
+            return state.map(todo => {
+                if (todo.id !== action.id) {
+                    return todo;
+                }
+
+                return Object.assign({}, todo, {
+                    completed: !todo.completed
+                });
+            });
         default:
             return state;
     }
@@ -40,5 +50,44 @@ const testAddTodo = () => {
     ).toEqual(stateAfter);
 };
 
+const testToggleTodo = () => {
+    const stateBefore = [
+        {
+            id: 0,
+            text: 'Log Time',
+            completed: false
+        },
+        {
+            id: 1,
+            text: 'Break time',
+            completed: false
+        }
+    ];
+    const action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+    };
+    const stateAfter = [
+        {
+            id: 0,
+            text: 'Log Time',
+            completed: false
+        },
+        {
+            id: 1,
+            text: 'Break time',
+            completed: true
+        }
+    ];
+
+    deepfreeze(stateBefore);
+    deepfreeze(action);
+
+    expect(
+        todos(stateBefore, action)
+    ).toEqual(stateAfter);
+};
+
 testAddTodo();
+testToggleTodo();
 console.log('All tests passed.');
