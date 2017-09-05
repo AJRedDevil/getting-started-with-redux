58ca59935272b3514fadf9cddf025ecf0c16f690
AJRedDevil <ajan.shresh@gmail.com>
2017-09-05 09:48:24 +0545

    Working of combineReducers of redux

--- playground/example-todolist.js
+++ playground/example-todolist.js
@@ -50,7 +50,22 @@ const visibilityFilter = (
     }
 };
 
-const { combineReducers } = redux;
+const combineReducers = (reducers) => {
+    return (state = {}, action) => {
+        return Object.keys(reducers).reduce(
+            (nextState, key) => {
+                nextState[key] = reducers[key](
+                    state[key],
+                    action
+                );
+                return nextState;
+            },
+            {}
+        );
+    };
+};
+
+// const { combineReducers } = redux;
 const todoApp = combineReducers({
     todos,
     visibilityFilter
