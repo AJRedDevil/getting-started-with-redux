0dad2d14e45144cdf2e97053edcc6183bd8ff25c
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2017-10-25 14:23:29 +0545

    Reducer composition with combineReducers to generate single reducer that calls many many reducers to manage each state by calling respective reducer.

--- src/reducer/todoList.js
+++ src/reducer/todoList.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 const todo = (state, action) => {
     switch(action.type) {
         case 'ADD_TODO':
@@ -45,15 +47,20 @@ const visibilityFilter = (
     }
 };
 
-const todoApp = (state = {}, action) => {
-    return {
-        todos: todos(
-            state.todos,
-            action
-        ),
-        visibilityFilter: visibilityFilter(
-            state.visibilityFilter,
-            action
-        )
-    };
-};
+const todoApp = combineReducers({
+    todos,
+    visibilityFilter
+});
+
+// const todoApp = (state = {}, action) => {
+//     return {
+//         todos: todos(
+//             state.todos,
+//             action
+//         ),
+//         visibilityFilter: visibilityFilter(
+//             state.visibilityFilter,
+//             action
+//         )
+//     };
+// };
