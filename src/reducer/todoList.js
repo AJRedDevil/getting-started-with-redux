fd59e2b922ac6d3051da0bfcd2c19336138d9679
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2017-10-25 14:17:03 +0545

    Redux reducer composition with objects

--- src/reducer/todoList.js
+++ src/reducer/todoList.js
@@ -32,3 +32,28 @@ export const todos = (state = [], action) => {
             return state;
     }
 };
+
+const visibilityFilter = (
+    state = 'SHOW_ALL',
+    action
+) => {
+    switch(action.type) {
+        case 'SET_VISIBILITY_FILTER':
+            return action.filter;
+        default:
+            return state
+    }
+};
+
+const todoApp = (state = {}, action) => {
+    return {
+        todos: todos(
+            state.todos,
+            action
+        ),
+        visibilityFilter: visibilityFilter(
+            state.visibilityFilter,
+            action
+        )
+    };
+};
