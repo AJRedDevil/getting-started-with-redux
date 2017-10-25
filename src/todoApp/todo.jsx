fb2f0d350697db7b2dc4e3ca66bd3c955f87d66d
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2017-10-25 16:28:30 +0545

    Extracting presentational components todo and todolist

---
+++ src/todoApp/todo.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// Presentational Component
+const Todo = ({
+    onClick,
+    completed,
+    text
+}) => (
+    <li
+        onClick={onClick}
+        style={{
+            textDecoration:
+                completed ?
+                    'line-through' :
+                    'none'
+        }}
+    >
+        {text}
+    </li>
+);
+
+export default Todo;
