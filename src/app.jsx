bf3db94a623591c0290c7a19b01d3bc21bc7e2d4
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2017-10-25 10:12:38 +0545

    Initial state

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,10 @@
 import React, { Component }  from 'react';
 
-import TodoApp from './todoApp/todo-app';
-
 class App extends Component {
     render() {
         return (
             <div>
-                <TodoApp
-                    store={this.props.store}
-                />
+                Hello World!
             </div>
         );
     };
--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 
 import App from './app';
-import { todoApp } from './todoApp/redux/reducer';
-
-const redux = require('redux');
-const { createStore } = redux;
-const store = createStore(
-    todoApp,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <App />,
     document.getElementById('root')
 );
